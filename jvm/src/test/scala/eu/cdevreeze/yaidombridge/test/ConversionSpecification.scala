29860d975cb5cfef1b9ba57600350496c3d7c60f
Chris de Vreeze <chris.de.vreeze@caiway.net>
2020-05-08 15:46:14 +0200

    More tests

--- jvm/src/test/scala/eu/cdevreeze/yaidombridge/test/ConversionSpecification.scala
+++ jvm/src/test/scala/eu/cdevreeze/yaidombridge/test/ConversionSpecification.scala
@@ -85,6 +85,37 @@ object ConversionSpecification extends Properties("conversions") {
     yaidom2.node.resolved.Elem.from(convertedDoc.documentElement) == yaidom2.node.resolved.Elem.from(doc1.documentElement)
   }
 
+  property("Conversion of indexed elem to yaidom2 retains equality") = forAll { (elem: yaidom.saxon.SaxonElem) =>
+    val elem1: yaidom.indexed.Elem =
+      yaidom.indexed.Elem(elem.docUri, SaxonElemToSimpleElemConverter.convertSaxonElem(elem.rootElem), elem.path)
+    val convertedElem = elem1.toYaidom2
+
+    yaidom2.node.resolved.Elem.from(convertedElem) == yaidom.resolved.Elem.from(elem1).pipe(_.toYaidom2)
+  }
+
+  property("Conversion of indexed elem to yaidom retains equality") = forAll { (elem: yaidom2.node.saxon.Elem) =>
+    val elem1 = yaidom2.node.indexed.Elem.from(elem)
+    val convertedElem = elem1.toYaidom
+
+    yaidom.resolved.Elem.from(convertedElem) == yaidom2.node.resolved.Elem.from(elem1).pipe(_.toYaidom)
+  }
+
+  property("Conversion of indexed elem to yaidom2 and back retains equality") = forAll { (elem: yaidom.saxon.SaxonElem) =>
+    val elem1: yaidom.indexed.Elem =
+      yaidom.indexed.Elem(elem.docUri, SaxonElemToSimpleElemConverter.convertSaxonElem(elem.rootElem), elem.path)
+    val convertedElem = elem1.toYaidom2.toYaidom
+
+    yaidom.resolved.Elem.from(convertedElem) == yaidom.resolved.Elem.from(elem1) && convertedElem.path == elem1.path
+  }
+
+  property("Conversion of indexed elem to yaidom and back retains equality") = forAll { (elem: yaidom2.node.saxon.Elem) =>
+    val elem1 = yaidom2.node.indexed.Elem.from(elem)
+    val convertedElem = elem1.toYaidom.toYaidom2
+
+    yaidom2.node.resolved.Elem.from(convertedElem) == yaidom2.node.resolved.Elem
+      .from(elem1) && convertedElem.ownNavigationPathRelativeToRootElem == elem1.ownNavigationPathRelativeToRootElem
+  }
+
   private val processor: Processor = new Processor(false)
 
   private val xmlFiles: Seq[File] = {
@@ -118,6 +149,38 @@ object ConversionSpecification extends Properties("conversions") {
       for {
         i <- Gen.oneOf(0.until(docs.size))
       } yield docs(i)
+
+    Arbitrary(result)
+  }
+
+  implicit private val anyYaidomSaxonElem: Arbitrary[yaidom.saxon.SaxonElem] = {
+    val docBuilder = processor.newDocumentBuilder()
+
+    val docs: Seq[yaidom.saxon.SaxonDocument] = xmlFiles
+      .map(f => docBuilder.build(f))
+      .map(xdmNode => yaidom.saxon.SaxonDocument.wrapDocument(xdmNode.getUnderlyingNode.getTreeInfo))
+
+    val result: Gen[yaidom.saxon.SaxonElem] =
+      for {
+        i <- Gen.oneOf(0.until(docs.size))
+        e <- Gen.oneOf(docs(i).documentElement.findAllElemsOrSelf)
+      } yield e
+
+    Arbitrary(result)
+  }
+
+  implicit private val anyYaidom2SaxonElem: Arbitrary[yaidom2.node.saxon.Elem] = {
+    val docBuilder = processor.newDocumentBuilder()
+
+    val docs: Seq[yaidom2.node.saxon.SaxonDocument] = xmlFiles
+      .map(f => docBuilder.build(f))
+      .map(xdmNode => yaidom2.node.saxon.SaxonDocument(xdmNode))
+
+    val result: Gen[yaidom2.node.saxon.Elem] =
+      for {
+        i <- Gen.oneOf(0.until(docs.size))
+        e <- Gen.oneOf(docs(i).documentElement.findAllDescendantElemsOrSelf)
+      } yield e
 
     Arbitrary(result)
   }
